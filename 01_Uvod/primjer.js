4b44c40e8cd1f847cc24229e7308b00e2af07a8b
Jasenka85 <132337552+Jasenka85@users.noreply.github.com>
2023-09-27 00:42:06 +0200

    Prvi koraci - Foundation

---
+++ 01_Uvod/primjer.js
@@ -0,0 +1,17 @@
+// alert('Hello world');
+
+// document.write('Hello world');
+
+console.log('Hello world');
+
+for(i=0; i<100; i++)
+{
+console.log('Osijek'+i);
+}
+
+let ime='pero';
+
+console.log(typeof(ime));
+
+ime=10;
+
